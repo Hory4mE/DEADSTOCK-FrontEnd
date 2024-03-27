563e4cfacc4671b28ec2ca5f0a32c41dad069ab8
Hory*- <64050156@KMITL.AC.TH>
2024-03-27 22:00:19 +0700

    Move Design : Payment[60%]

--- src/Components/ForPayment/PaymentForm.js
+++ src/Components/ForPayment/PaymentForm.js
@@ -5,7 +5,19 @@ import BillingAddressSection from './BillingAddressSection';
 import SummarySection from './SummarySection';
 import DifferentBillingAddress from './DifferentBillingAddress';
 
-function PaymentForm({ products }) {
+function PaymentForm(/*{ products } ตรงนี้เป็น dynamic*/) {
+
+    //static product
+    const products = [
+        {
+            id: 1,
+            name: "712Puma Sweater",
+            size: "XL",
+            image: "https://cdn.builder.io/api/v1/image/assets/TEMP/96b9fc1089ec1bec9b688e9d6466d82e10453b39411a53936e27c35e94da6853?apiKey=c3d84cbd0c3a42f4a1616e4ea278d805&",
+        },
+    ];
+
+
     const [showBillingAddress, setShowBillingAddress] = useState(false);
     const [useSameAddress, setUseSameAddress] = useState(false);
 
@@ -19,19 +31,21 @@ function PaymentForm({ products }) {
 
     return (
         <div className="max-w-screen-lg mx-auto px-10 mt-36 max-md:flex-wrap max-md:mt-10">
-            <div className="flex justify-between items-start">
-                <div className="flex-1 mr-5">
+            <div className="flex justify-between items-start" style={{ width: '60%' }}>
+                <div className="flex-3 mr-5">
                     <ContactSection />
                     <DeliverySection />
-                    <DifferentBillingAddress 
-                        onToggleBillingAddress={handleToggleBillingAddress} 
-                        onToggleSameAddress={handleToggleSameAddress} 
+                    <DifferentBillingAddress
+                        onToggleBillingAddress={handleToggleBillingAddress}
+                        onToggleSameAddress={handleToggleSameAddress}
                     />
-                    <BillingAddressSection 
-                        showBillingAddress={showBillingAddress && !useSameAddress} 
+                    <BillingAddressSection
+                        showBillingAddress={showBillingAddress && !useSameAddress}
                     />
                 </div>
-                <SummarySection products={products} />
+                <div className="flex-1">
+                    <SummarySection products={products} />
+                </div>
             </div>
         </div>
     );
