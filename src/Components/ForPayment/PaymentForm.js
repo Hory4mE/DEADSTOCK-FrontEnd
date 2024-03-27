c306034cdb47f739e4ec691abf3649f445801a7b
Hory*- <64050156@KMITL.AC.TH>
2024-03-27 21:39:42 +0700

    Modify Payment Page : Redesign

--- src/Components/ForPayment/PaymentForm.js
+++ src/Components/ForPayment/PaymentForm.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import ContactSection from './ContactSection';
 import DeliverySection from './DeliverySection';
 import BillingAddressSection from './BillingAddressSection';
@@ -5,13 +6,33 @@ import SummarySection from './SummarySection';
 import DifferentBillingAddress from './DifferentBillingAddress';
 
 function PaymentForm({ products }) {
+    const [showBillingAddress, setShowBillingAddress] = useState(false);
+    const [useSameAddress, setUseSameAddress] = useState(false);
+
+    const handleToggleBillingAddress = (show) => {
+        setShowBillingAddress(show);
+    };
+
+    const handleToggleSameAddress = (same) => {
+        setUseSameAddress(same);
+    };
+
     return (
-        <div className="flex gap-5 justify-between items-start self-center px-5 mt-36 max-md:flex-wrap max-md:mt-10">
-            <ContactSection />
-            <DeliverySection />
-            <BillingAddressSection />
-            <DifferentBillingAddress />
-            <SummarySection products={products} />
+        <div className="max-w-screen-lg mx-auto px-10 mt-36 max-md:flex-wrap max-md:mt-10">
+            <div className="flex justify-between items-start">
+                <div className="flex-1 mr-5">
+                    <ContactSection />
+                    <DeliverySection />
+                    <DifferentBillingAddress 
+                        onToggleBillingAddress={handleToggleBillingAddress} 
+                        onToggleSameAddress={handleToggleSameAddress} 
+                    />
+                    <BillingAddressSection 
+                        showBillingAddress={showBillingAddress && !useSameAddress} 
+                    />
+                </div>
+                <SummarySection products={products} />
+            </div>
         </div>
     );
 }
